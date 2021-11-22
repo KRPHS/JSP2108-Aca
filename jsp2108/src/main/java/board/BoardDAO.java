79a96bc8aab20cb4822f34dc40b363d8a2a48315
PHS <qgt0310@naver.com>
2021-11-22 17:51:08 +0900

    211122 1751

---
+++ jsp2108/src/main/java/board/BoardDAO.java
@@ -0,0 +1,52 @@
+package board;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import conn.GetConn;
+
+public class BoardDAO {
+	GetConn getConn = GetConn.getInstance();
+	private Connection conn = getConn.getConn();
+	private PreparedStatement pstmt = null;
+	private ResultSet rs = null;
+	
+	private String sql = "";
+	
+	BoardVO vo = null;
+
+	public List<BoardVO> getList() {
+		List<BoardVO> vos = new ArrayList<BoardVO>();
+		try {
+			sql = "select * from board order by idx desc";
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				vo = new BoardVO();
+				vo.setIdx(rs.getInt("idx"));
+				vo.setNickName(rs.getString("nickName"));
+				vo.setTitle(rs.getString("title"));
+				vo.setEmail(rs.getString("email"));
+				vo.setHomePage(rs.getString("homePage"));
+				vo.setContent(rs.getString("content"));
+				vo.setwDate(rs.getString("wDate"));
+				vo.setReadNum(rs.getInt("readNum"));
+				vo.setHostIp(rs.getString("hostIp"));
+				vo.setGoods(rs.getInt("goods"));
+				vo.setMid(rs.getString("mid"));
+				vos.add(vo);
+			}
+			
+			
+		} catch (SQLException e) {
+			System.out.println("SQL에러 :: " + e.getMessage());
+		} finally {
+			getConn.rsClose();
+		}
+		return vos;
+	}
+}
